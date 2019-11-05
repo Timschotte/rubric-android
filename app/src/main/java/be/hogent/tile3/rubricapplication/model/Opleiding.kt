308e73ea608d71ff371504ef57d2d710ce691115
Arjen Trinquet <arjen_trinquet@hotmail.com>
2019-11-05 22:11:18 +0100

    ViewModel + models implementation in progress

---
+++ app/src/main/java/be/hogent/tile3/rubricapplication/model/Opleiding.kt
@@ -0,0 +1,18 @@
+package be.hogent.tile3.rubricapplication.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "opleiding_table")
+data class Opleiding(
+
+    @PrimaryKey(autoGenerate = true)
+    var opleidingId: Int = 0,
+
+    @ColumnInfo(name = "naam")
+    var naam: String,
+
+    @ColumnInfo(name = "rubrics")
+    var rubrics: Rubric[]
+)
--- app/src/main/java/be/hogent/tile3/rubricapplication/model/Student.kt
+++ app/src/main/java/be/hogent/tile3/rubricapplication/model/Student.kt
@@ -2,11 +2,25 @@ package be.hogent.tile3.rubricapplication.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity( tableName = "student_table")
+@Entity( tableName = "student_table", foreignKeys = arrayOf(ForeignKey(
+    entity = Opleiding::class,
+    parentColumns = arrayOf("student_id"),
+    childColumns = arrayOf("opleiding_id")
+)))
 data class Student(
-    @PrimaryKey @ColumnInfo(name = "studentId") val studentId: String,
-    @ColumnInfo(name = "studentNaam") val studentNaam: String,
-    @ColumnInfo(name = "studentNr") val studentenNr: String
+
+    @PrimaryKey @ColumnInfo(name = "student_id")
+    val studentId: String,
+
+    @ColumnInfo(name = "studentNaam")
+    val studentNaam: String,
+
+    @ColumnInfo(name = "studentNr")
+    val studentenNr: String,
+
+    @ColumnInfo(name = "opleiding_id")
+    val opleidingId: Int
 )
