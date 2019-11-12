f371efb309204cb2eea0dcd9c73e4008f67d0e52
Aspers <32874385+aspers@users.noreply.github.com>
2019-11-12 23:52:04 +0100

    Add model binding to RV and add Binding adapter

--- app/src/main/java/be/hogent/tile3/rubricapplication/adapters/OpleidingsOnderdeelListAdapter.kt
+++ app/src/main/java/be/hogent/tile3/rubricapplication/adapters/OpleidingsOnderdeelListAdapter.kt
@@ -3,17 +3,15 @@ package be.hogent.tile3.rubricapplication.adapters
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import be.hogent.tile3.rubricapplication.R
 import be.hogent.tile3.rubricapplication.model.OpleidingsOnderdeel
 import be.hogent.tile3.rubricapplication.databinding.OpleidingTextViewBinding
 
 //TODO: Recyclerview voor opleidingen fixen
 
-class OpleidingsOnderdeelListAdapter: ListAdapter<OpleidingsOnderdeel, OpleidingViewHolder>(OpleidingsOnderdeelDiffCallback()){
+class OpleidingsOnderdeelListAdapter: ListAdapter<OpleidingsOnderdeel, OpleidingsOnderdeelListAdapter.OpleidingViewHolder>(OpleidingsOnderdeelDiffCallback()){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OpleidingViewHolder {
         Log.i("OpleidingsOnderdeelLA", "Creating viewholder...")
@@ -24,24 +22,24 @@ class OpleidingsOnderdeelListAdapter: ListAdapter<OpleidingsOnderdeel, Opleiding
         val item = getItem(position)
         holder.bind(item)
     }
-}
 
-class OpleidingViewHolder private constructor(val binding: OpleidingTextViewBinding) : RecyclerView.ViewHolder(binding.root) {
-    val opleidingsOnderdeel: TextView = itemView.findViewById(R.id.opleiding_naam)
+    class OpleidingViewHolder private constructor(val binding: OpleidingTextViewBinding) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(item: OpleidingsOnderdeel) {
-        Log.i("OpleidingsOnderdeelLA", "Binding text..." + item.naam)
-        opleidingsOnderdeel.setText(item.naam)
-    }
+        fun bind(item: OpleidingsOnderdeel) {
+            binding.opleidingsOnderdeel = item
+            binding.executePendingBindings()
+        }
 
-    companion object {
-        fun from(parent: ViewGroup): OpleidingViewHolder {
-            val layoutInflater = LayoutInflater.from(parent.context)
-            val binding = OpleidingTextViewBinding.inflate(layoutInflater, parent, false)
-            return OpleidingViewHolder(binding)
+        companion object {
+            fun from(parent: ViewGroup): OpleidingViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = OpleidingTextViewBinding.inflate(layoutInflater, parent, false)
+                return OpleidingViewHolder(binding)
+            }
         }
     }
 }
+
 
 class OpleidingsOnderdeelDiffCallback: DiffUtil.ItemCallback<OpleidingsOnderdeel>() {
     override fun areItemsTheSame(oldItem: OpleidingsOnderdeel, newItem: OpleidingsOnderdeel): Boolean {
