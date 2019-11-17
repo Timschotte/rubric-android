882c0f544bb3a39d2e45a654e5f46c3a224e4bc4
Laurens Mertens <laurencemertens@gmail.com>
2019-11-17 11:22:57 +0100

    Aanmaken netwerkDtos voor Evaluatie (POST)

---
+++ app/src/main/java/be/hogent/tile3/rubricapplication/network/EvaluatieDataTransferObjects.kt
@@ -0,0 +1,17 @@
+package be.hogent.tile3.rubricapplication.network
+
+data class NetworkRubricEvaluatie(
+    val commentaar: String,
+    val docentId: Int,
+    val studentId: Int,
+    val rubricId: Int,
+    val criteriumEvaluaties: List<NetworkCriteriumEvaluatie>
+)
+
+data class NetworkCriteriumEvaluatie(
+    val id: Int?,
+    val niveauScore: Int,
+    val commentaar: String,
+    val criteriumId: Int,
+    val behaaldNiveauId: Int
+)
