7f0e821f4cac48f4c825c2f1a7c3bc5dac569dcc
Laurens Mertens <laurencemertens@gmail.com>
2019-11-17 17:10:04 +0100

    Toevoegen knop 'Opslaan' in evaulatiescherm

--- app/src/main/java/be/hogent/tile3/rubricapplication/utils/Constants.kt
+++ app/src/main/java/be/hogent/tile3/rubricapplication/utils/Constants.kt
@@ -1,4 +1,7 @@
 package be.hogent.tile3.rubricapplication.utils
 
-const val BASE_URL: String = "http://35.210.250.75:8080/"
+//const val BASE_URL: String = "http://35.210.250.75:8080/"
+//TODO change this back before pull request
+const val BASE_URL: String = "http://192.168.0.240:8080/"
+
 const val TEMP_EVALUATIE_ID: Long = -1L
