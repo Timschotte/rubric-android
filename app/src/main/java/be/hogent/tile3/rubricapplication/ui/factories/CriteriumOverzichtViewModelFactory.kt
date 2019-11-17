48980b8c8c38dbc430a2a39410a0ae79078aa32d
Laurens Mertens <laurencemertens@gmail.com>
2019-11-17 22:05:15 +0100

    Aanmaken knop opslaan en fixen van clickhandler (waarbij viewmodel null was in het fragment)

--- app/src/main/java/be/hogent/tile3/rubricapplication/ui/factories/CriteriumOverzichtViewModelFactory.kt
+++ app/src/main/java/be/hogent/tile3/rubricapplication/ui/factories/CriteriumOverzichtViewModelFactory.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import be.hogent.tile3.rubricapplication.ui.CriteriumOverzichtViewModel
 
-class CriteriumOverzichtViewModelFactory (private val rubricId: String, private val studentId: Long) : ViewModelProvider.Factory {
+class CriteriumOverzichtViewModelFactory (private val rubricId: String, private val studentId: Long, private val evaluatieId: Long?) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CriteriumOverzichtViewModel::class.java)) {
-            return CriteriumOverzichtViewModel(rubricId, studentId) as T
+            return CriteriumOverzichtViewModel(rubricId, studentId, evaluatieId) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
