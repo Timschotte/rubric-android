50ed0e13ed134b38fcc60308095440f35a3735e4
Laurens Mertens <laurencemertens@gmail.com>
2019-11-18 11:43:16 +0100

    Evaluatie en CriteriumEvaluaties worden geïnitialiseerd bij het openen van het fragment (indien nodig)

--- app/src/main/java/be/hogent/tile3/rubricapplication/fragments/CriteriumOverzichtFragment.kt
+++ app/src/main/java/be/hogent/tile3/rubricapplication/fragments/CriteriumOverzichtFragment.kt
@@ -29,7 +29,6 @@ import be.hogent.tile3.rubricapplication.utils.TEMP_EVALUATIE_ID
 
 class CriteriumOverzichtFragment : Fragment() {
 
-    private var criteriumOverzichtViewModel: CriteriumOverzichtViewModel? = null
     private var alertDialog: AlertDialog? = null
 
     override fun onCreateView(
@@ -48,6 +47,16 @@ class CriteriumOverzichtFragment : Fragment() {
         val viewModelFactory = CriteriumOverzichtViewModelFactory(args.rubricId, args.studentId)
         val criteriumOverzichtViewModel = ViewModelProviders.of(this, viewModelFactory)
             .get(CriteriumOverzichtViewModel::class.java)
+
+        criteriumOverzichtViewModel.rubricCriteria.observe(viewLifecycleOwner, Observer {
+            it?.let{
+                if(it.isNotEmpty()){
+                    criteriumOverzichtViewModel.initaliseerWanneerNodig()
+                }
+            }
+        })
+
+
 
         Toast.makeText(
             context,
