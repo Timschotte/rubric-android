db86b05a4655fc1a3deb23f4b1d393594161439d
Laurens Mertens <laurencemertens@gmail.com>
2020-01-25 15:17:48 +0100

    Removing constraints on table StudentOpleidingsOnderdeel

--- app/src/main/java/be/hogent/tile3/rubricapplication/model/StudentOpleidingsOnderdeel.kt
+++ app/src/main/java/be/hogent/tile3/rubricapplication/model/StudentOpleidingsOnderdeel.kt
@@ -11,15 +11,7 @@ import androidx.room.ForeignKey
  */
 @Entity(
     tableName = "student_opleidingsonderdeel_table",
-    primaryKeys = ["studentId", "opleidingsOnderdeelId"],
-    foreignKeys = [
-        ForeignKey(entity = Student::class,
-            parentColumns = ["studentId"],
-            childColumns = ["studentId"]),
-        ForeignKey(entity = OpleidingsOnderdeel::class,
-            parentColumns = ["opleidingsOnderdeelId"],
-            childColumns = ["opleidingsOnderdeelId"])
-    ])
+    primaryKeys = ["studentId", "opleidingsOnderdeelId"])
 data class StudentOpleidingsOnderdeel(
     val studentId: Long,
     val opleidingsOnderdeelId: Long
